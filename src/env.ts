49045494c2d2bb5a0d53e73b1570160669def403
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-02-25 11:50:35 +0100

    chore: update env file with auto env requirement

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,31 @@
-const { PORT = '3000', NODE_ENV = 'development', DATABASE_URL = './data/db.sqlite' } = process.env
+// Description: This file contains the environment variables for the application.
 
-const env = { PORT, NODE_ENV, DATABASE_URL }
+// Require the necessary environment variables
+interface Env {
+  PORT: string
+  NODE_ENV: string
+  DATABASE_URL: string
+}
 
-export default env
+const {
+  PORT = '3000',
+  NODE_ENV = 'development',
+  DATABASE_URL = './data/db.sqlite',
+  ...env
+} = process.env
+
+// Add required keys here
+const requiredKeys: (keyof Env)[] = []
+
+const validatedEnv: Partial<Env> = {
+  PORT,
+  DATABASE_URL,
+  NODE_ENV,
+}
+
+requiredKeys.forEach((key: keyof Env) => {
+  if (!env[key]) throw new Error(`${key} is required`)
+  validatedEnv[key] = env[key]
+})
+
+export default validatedEnv as Env
